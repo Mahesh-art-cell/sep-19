734c2916b23805ca1d0661664d3681c976009374
Mahesh-art-cell <tallurim886@email.com>
2024-09-25 19:39:03 +0530

    add the all files

---
+++ day3.js
@@ -0,0 +1,20 @@
+var a=10;
+console.log(a);
+
+{
+    let b=20;
+    console.log(b);
+    var x=40
+    console.log(x);
+    
+}
+
+{
+    const c=30;
+    console.log(c);
+    var y=50;
+    console.log(y);
+    
+}
+
+
